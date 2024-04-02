cc3e192279d708249eef55587320a44bb4d45e77
ZhangYuKai <2197640997@qq.com>
2024-04-02 18:01:07 +0800

    测试

--- service/src/main/java/edu/cdtu/ShopService.java
+++ service/src/main/java/edu/cdtu/ShopService.java
@@ -1,4 +1,5 @@
 package edu.cdtu;
 
 public interface ShopService {
+
 }
--- web/src/main/java/edu/cdtu/config/mybatis/MyBatisPlusConfig.java
+++ web/src/main/java/edu/cdtu/config/mybatis/MyBatisPlusConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@MapperScan("edu.cdtu.*.*.mapper")
+@MapperScan("edu.cdtu.*.*.mapper")
 public class MyBatisPlusConfig {
     // 最新版分页插件
     @Bean
