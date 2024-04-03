a529040706254515d6247398b5ec7e3b039232e4
YangSuGuo <3130876713@qq.com>
2024-04-03 12:42:16 +0800

    取消推送配置文件

--- api/src/main/java/edu/cdtu/controller/SysUserController.java
+++ api/src/main/java/edu/cdtu/controller/SysUserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/sysusers")
+@RequestMapping("/api/v1")
 public class SysUserController {
 
     @Autowired
